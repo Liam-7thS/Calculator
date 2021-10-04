b059e2e1a98850f6909d50df00182249810f942a
David2561 <davidtkf256@gmail.com>
2021-10-04 18:09:08 -0300

    Add ui wiring

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,65 @@
 const upDisplay = document.querySelector('.Upper-display');
 const loDisplay = document.querySelector('.Lower-display');
-const nums = document.querySelectorAll('.Number');
-let a = 0;
-let b = 0;
+const bttns = document.querySelectorAll('.Number');
+const c = document.querySelector('#C');
+let a = '0';
+let b = '0';
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 
+window.addEventListener('keydown', function(e){
+
+    const key = document.querySelector(`[data-val="${e.key}"]`);
+    if(key){
+        keyInputManager(key.getAttribute("data-val"));
+    }
+
+} );
+window.addEventListener('click',function(e){
+    const key = e.target;
+    if(key){
+        keyInputManager(key.getAttribute("data-val"));
+    }
+} );
+c.addEventListener('click',ClearAll);
+
+
+function keyInputManager(val){
+    if (val !== null && val !== undefined){
+        if (val >= 0)
+    {
+        Read(val);
+    }
+    else{
+        console.log("you clicked an operator: " + val)
+    }
+    }
+    
+
+}
+function Read(val){
+    if (a === '0')
+    {
+        a = val;
+    }
+    else{
+        a += val;
+    }
+    
+    loDisplay.textContent = a;
+}
+
+
+function ClearAll(){
+    a = '0';
+    b = '0';
+    upDisplay.textContent = a;
+    loDisplay.textContent = b;
+}
+function ClearUpper(){
+    a = '0'
+}
 function Add(a,b){
     return a + b;
 }
